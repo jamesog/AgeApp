e9c711f551055404e685aa9c99103dbad80c05bf
James O'Gorman <james@netinertia.co.uk>
2023-03-29 20:24:00 +0100

    Prototype basic layout
    Throwing some ideas around for the layout of the app.
    
    There's a "tabbed" UI using a segmented picker to choose between
    password-based encryption and pubkey-based encryption. Choosing between
    the picker selections changes between a password input box, and a button
    for adding recipient pubkeys.
    
    There's a bug in the latter: adding a recipient pushes a new entry into
    the recipients array but for some reason trying to enter text into the
    TextField only lets you type one character at a time.
    
    There's some magic with using DispatchQueue.main.asyncAfter in a Task to
    let you bring focus to the recipient field, found from Stewart Lynch's
    YouTube channel.
    
    I've imported the official Age logos which work perfectly in dark and
    light mode.

--- Age/ContentView.swift
+++ Age/ContentView.swift
@@ -1,19 +1,88 @@
-//
-//  ContentView.swift
-//  Age
-//
-//  Created by James O'Gorman on 12/04/2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var plaintext = ""
+    @State private var method: Methods = .scrypt
+    enum Methods: String, CaseIterable {
+        case scrypt = "Password"
+        case x25519 = "Public keys"
+    }
+
+    @State private var password = ""
+    @State private var recipients: [String] = []
+    @FocusState private var recipientFocused: Bool
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("Logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 100)
+
+            Spacer()
+
+            Form {
+                Section {
+                    Picker("Encryption method", selection: $method.animation()) {
+                        ForEach(Methods.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+
+                    switch method {
+                    case .scrypt :
+                        SecureField("Password", text: $password)
+                    case .x25519:
+                        List($recipients, id: \.self) {
+                            TextField("Recipient", text: $0)
+                                .focused($recipientFocused)
+                        }
+                        Button {
+                            withAnimation {
+                                recipients.append("")
+                            }
+
+                        } label: {
+                            HStack {
+                                Image(systemName: "plus.circle")
+                                    .foregroundColor(.green)
+
+                                Text("Add recipient")
+                            }
+                        }
+                        .task {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                recipientFocused = true
+                            }
+                        }
+                    }
+                    TextField("Plaintext", text: $plaintext, axis: .vertical)
+                        #if os(iOS)
+                        .textInputAutocapitalization(.never)
+                        #endif
+                        .autocorrectionDisabled()
+                        .lineLimit(10)
+
+                }
+                Section {
+                    Button("Encrypt") {
+
+                    }
+                    .disabled(plaintext.isEmpty || password.isEmpty)
+                }
+            }
+
+            Spacer()
+
+            HStack {
+                Spacer()
+                Button {
+
+                } label: {
+                    Image(systemName: "gear")
+                }
+            }
         }
         .padding()
     }
