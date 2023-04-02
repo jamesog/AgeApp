49ee2d5999a24cdfc0f43ef1f1c93d455e616011
James O'Gorman <james@netinertia.co.uk>
2023-04-02 19:53:28 +0100

    EncryptView: Implement Scrypt encryption
    Take password and plaintext input, create an Age ScryptRecipient and
    pass it through Age.encrypt, finally creating an armoured output into a
    text box that copies to the clipboard on tap.
    
    A share sheet is also added to easily send the armoured output to
    another app or save to a file.

--- Age/EncryptView.swift
+++ Age/EncryptView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import AgeKit
 
 struct EncryptView: View {
     @State private var plaintext = ""
@@ -7,6 +8,21 @@ struct EncryptView: View {
     @State private var password = ""
     @State private var recipients: [String] = []
     @FocusState private var recipientFocused: Bool
+
+    @State private var encrypting = false
+    @State private var ciphertext = ""
+    @State private var copied = false
+
+    private let pasteboard = UIPasteboard.general
+
+    private var disableButton: Bool {
+        switch method {
+        case .scrypt:
+            return password.isEmpty || plaintext.isEmpty
+        case .x25519:
+            return recipients.isEmpty || plaintext.isEmpty
+        }
+    }
 
     var body: some View {
         Form {
@@ -55,11 +71,71 @@ struct EncryptView: View {
             }
             Section {
                 Button("Encrypt") {
-
+                    encrypting = true
+                    ciphertext = ""
+                    Task {
+                        await encrypt()
+                        password = ""
+                    }
+                    encrypting = false
+                    copied = false
                 }
-                .disabled(plaintext.isEmpty || password.isEmpty)
+                .disabled(disableButton)
+            }
+
+            if !ciphertext.isEmpty {
+                Section("Output") {
+                    Text(ciphertext)
+                        .font(.system(size: 12, design: .monospaced))
+                        .textSelection(.enabled)
+                        .onTapGesture {
+                            withAnimation {
+                                pasteboard.string = ciphertext
+                                copied = true
+                            }
+                        }
+
+                    if copied {
+                        Label("Copied", systemImage: "doc.on.clipboard")
+                            .foregroundColor(.accentColor)
+                            .font(.subheadline)
+                    }
+                }
+                Section {
+                    ShareLink(item: ciphertext)
+                }
             }
         }
+    }
+}
+
+extension EncryptView {
+    func recipient(password: String) -> Age.ScryptRecipient? {
+        Age.ScryptRecipient(password: password)
+    }
+
+    func encrypt() async {
+        var out = OutputStream.toMemory()
+        out.open()
+        if let r = recipient(password: password) {
+            let w = try? Age.encrypt(dst: &out, recipients: r)
+            guard var w else {
+                debugPrint("Encrypt: couldn't create output")
+                return
+            }
+            _ = try! w.write(plaintext)
+        }
+        out.close()
+        let enc = out.property(forKey: .dataWrittenToMemoryStreamKey) as! Data
+
+        out = OutputStream.toMemory()
+        out.open()
+        var w = Armor.Writer(dst: out)
+        _ = try! w.write(enc)
+        try! w.close()
+        out.close()
+        let d = out.property(forKey: .dataWrittenToMemoryStreamKey) as! Data
+        ciphertext = String(data: d, encoding: .ascii)!
     }
 }
 
